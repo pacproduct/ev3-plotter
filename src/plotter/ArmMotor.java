f584090e48a075efe256e81d47b19bb4b8667ecb
PAC <charrierpa@gmail.com>
2015-08-09 00:28:13 +0200

    Implementation of the Sync between 2 'ArmMotor' + Code cleaning and commenting.

--- src/plotter/ArmMotor.java
+++ src/plotter/ArmMotor.java
@@ -1,20 +1,34 @@
 package plotter;
 
+import java.util.ArrayList;
+
 import lejos.hardware.motor.BaseRegulatedMotor;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.Port;
+import lejos.robotics.RegulatedMotor;
 
 public class ArmMotor {
-	private BaseRegulatedMotor[] allMotors;
-	private BaseRegulatedMotor[] slaveMotors;
+	// Lists of all the motors this ArmMotor is made of.
+	private ArrayList<BaseRegulatedMotor> allMotors = new ArrayList<BaseRegulatedMotor>();
+	// List of all motors, minus the master one.
+	private ArrayList<BaseRegulatedMotor> slaveMotors = new ArrayList<BaseRegulatedMotor>();
 
 	private int directionOperator = 1;
 
+	// -- Max and min position variables --
+	// Boolean indicating whether max and min positions are active or not.
 	private boolean hasAMaximumPosition = false;
 	private boolean hasAMinimumPosition = false;
+	// Max and min position.
 	private int maximumPosition = 0;
 	private int minimumPosition = 0;
+
+	// -- Arm synchronization variables --
+	// This turns to true when an Arm sync has been started.
+	private boolean armSyncIsOn = false;
+	// Complete list of synchronized slave motors when syncing 2 arms.
+	private ArrayList<BaseRegulatedMotor> crossArmSynchronizedSlaveMotors = new ArrayList<BaseRegulatedMotor>();
 
 	/**
 	 * Constructor.
@@ -26,165 +40,300 @@ public class ArmMotor {
 	 *            Indicates whereas this motor should behave normally. Set it to
 	 *            true to behave the normal way, or false to behave in reverse
 	 *            mode.
-	 * @param bigMotor
-	 *            Indicates whether this motor will be an
-	 *            EV3LargeRegulatedMotor, or a medium one. Set to true if using
-	 *            a large one.
+	 * @param useBigMotors
+	 *            Indicates whether this Arm uses EV3LargeRegulatedMotor motors,
+	 *            or medium ones. Set to true if it is using large ones.
 	 */
-	public ArmMotor(Port[] ports, boolean forward, boolean bigMotor) {
+	public ArmMotor(Port[] ports, boolean forward, boolean useBigMotors) {
 		if (forward) {
 			this.directionOperator = 1;
 		} else {
 			this.directionOperator = -1;
 		}
 
-		// Initialize array of motors.
-		this.allMotors = new BaseRegulatedMotor[ports.length];
-
-		if (bigMotor) {
+		if (useBigMotors) {
 			for (int i = 0; i < ports.length; i++) {
-				this.allMotors[i] = new EV3LargeRegulatedMotor(ports[i]);
+				this.allMotors.add(new EV3LargeRegulatedMotor(ports[i]));
 			}
 		} else {
 			for (int i = 0; i < ports.length; i++) {
-				this.allMotors[i] = new EV3MediumRegulatedMotor(ports[i]);
+				this.allMotors.add(new EV3MediumRegulatedMotor(ports[i]));
 			}
 		}
 
 		// Prepare list of slave motors.
-		this.slaveMotors = new BaseRegulatedMotor[ports.length - 1];
 		for (int i = 1; i < ports.length; i++) {
-			this.slaveMotors[i - 1] = this.allMotors[i];
+			this.slaveMotors.add(this.allMotors.get(i));
 		}
 	}
 
-	public void setMaximumPosition(int val) {
+	/**
+	 * Set a maximum angle for this AmrMotor. When set, asking the ArMotor to
+	 * rotate after this angle won't be possible anymore.
+	 *
+	 * @param angle
+	 *            Angle, in degrees.
+	 */
+	public void setMaximumPosition(int angle) {
 		this.hasAMaximumPosition = true;
-		this.maximumPosition = val;
+		this.maximumPosition = angle;
 	}
 
-	public void setMinimumPosition(int val) {
+	/**
+	 * Set a minimum angle for this AmrMotor. When set, asking the ArMotor to
+	 * rotate before this angle won't be possible anymore.
+	 *
+	 * @param angle
+	 *            Angle, in degrees.
+	 */
+	public void setMinimumPosition(int angle) {
 		this.hasAMinimumPosition = true;
-		this.minimumPosition = val;
+		this.minimumPosition = angle;
 	}
 
-	public void rotate(int val, boolean immediateReturn) {
+	/**
+	 * Rotates this ArmMotor by the given angle.
+	 *
+	 * @param angle
+	 *            Angle, in degrees.
+	 * @param immediateReturn
+	 *            If true, method returns immediately. If false, waits for the
+	 *            operation to complete.
+	 */
+	public void rotate(int angle, boolean immediateReturn) {
 		int currentPosition = this.getPosition();
 
-		this.rotateTo(currentPosition + val, immediateReturn);
+		this.rotateTo(currentPosition + angle, immediateReturn);
 	}
 
+	/**
+	 * Returns ArmMotor's current position (angle).
+	 *
+	 * @return The current angle of this ArmMotor, in degrees.
+	 */
 	public int getPosition() {
 		// Consider motor number 0 as the master.
-		return (int) this.allMotors[0].getPosition() * this.directionOperator;
+		return (int) this.allMotors.get(0).getPosition()
+				* this.directionOperator;
 	}
 
 	/*
 	 * Synchronized functions, abstracting access to physical motors.
 	 */
 
-	public void rotateTo(int val, boolean immediateReturn) {
+	/**
+	 * Rotates this ArmMotor by the given angle.
+	 *
+	 * @param angle
+	 *            Angle, in degrees.
+	 * @param immediateReturn
+	 *            If true, method returns immediately. If false, waits for the
+	 *            operation to complete.
+	 */
+	public void rotateTo(int angle, boolean immediateReturn) {
 		// Make sure value is within boundaries.
-		if (this.hasAMaximumPosition && val > this.maximumPosition) {
-			val = this.maximumPosition;
+		if (this.hasAMaximumPosition && angle > this.maximumPosition) {
+			angle = this.maximumPosition;
 		}
-		if (this.hasAMinimumPosition && val < this.minimumPosition) {
-			val = this.minimumPosition;
+		if (this.hasAMinimumPosition && angle < this.minimumPosition) {
+			angle = this.minimumPosition;
 		}
 
 		// Prepare value to rotate to.
-		int destValue = val * this.directionOperator;
+		int destValue = angle * this.directionOperator;
 
 		// Apply action, in a synchronized fashion.
 		this.synchronizeAllMotors();
-		for (int i = 0; i < allMotors.length; i++) {
-			this.allMotors[i].rotateTo(destValue, true);
+		for (int i = 0; i < allMotors.size(); i++) {
+			this.allMotors.get(i).rotateTo(destValue, true);
 		}
-		this.stopSyncAndLaunchSynchronizedOperations(immediateReturn);
+		this.stopSyncAndRunOperations(immediateReturn);
 	}
 
+	/**
+	 * Rotates this ArmMotor forward.
+	 *
+	 * @param immediateReturn
+	 *            If true, method returns immediately. If false, waits for the
+	 *            operation to complete. Note that if no maximum position was
+	 *            set prior to calling this function, this parameter won't have
+	 *            any effect and the function will return immediately.
+	 */
 	public void forward(boolean immediateReturn) {
 		if (this.hasAMaximumPosition) {
 			this.rotateTo(this.maximumPosition, immediateReturn);
 		} else {
 			if (this.directionOperator == 1) {
 				this.synchronizeAllMotors();
-				for (int i = 0; i < allMotors.length; i++) {
-					this.allMotors[i].forward();
+				for (int i = 0; i < allMotors.size(); i++) {
+					this.allMotors.get(i).forward();
 				}
-				this.stopSyncAndLaunchSynchronizedOperations(true);
+				this.stopSyncAndRunOperations(true);
 			} else {
 				this.synchronizeAllMotors();
-				for (int i = 0; i < allMotors.length; i++) {
-					this.allMotors[i].backward();
+				for (int i = 0; i < allMotors.size(); i++) {
+					this.allMotors.get(i).backward();
 				}
-				this.stopSyncAndLaunchSynchronizedOperations(true);
+				this.stopSyncAndRunOperations(true);
 			}
 		}
 	}
 
+	/**
+	 * Rotates this ArmMotor backward.
+	 *
+	 * @param immediateReturn
+	 *            If true, method returns immediately. If false, waits for the
+	 *            operation to complete. Note that if no maximum position was
+	 *            set prior to calling this function, this parameter won't have
+	 *            any effect and the function will return immediately.
+	 */
 	public void backward(boolean immediateReturn) {
 		if (this.hasAMinimumPosition) {
 			this.rotateTo(this.minimumPosition, immediateReturn);
 		} else {
 			if (this.directionOperator == 1) {
 				this.synchronizeAllMotors();
-				for (int i = 0; i < allMotors.length; i++) {
-					this.allMotors[i].backward();
+				for (int i = 0; i < allMotors.size(); i++) {
+					this.allMotors.get(i).backward();
 				}
-				this.stopSyncAndLaunchSynchronizedOperations(true);
+				this.stopSyncAndRunOperations(true);
 			} else {
 				this.synchronizeAllMotors();
-				for (int i = 0; i < allMotors.length; i++) {
-					this.allMotors[i].forward();
+				for (int i = 0; i < allMotors.size(); i++) {
+					this.allMotors.get(i).forward();
 				}
-				this.stopSyncAndLaunchSynchronizedOperations(true);
+				this.stopSyncAndRunOperations(true);
 			}
 		}
 	}
 
+	/**
+	 * Resets the tachometer of this ArmMotor.
+	 */
 	public void resetTachoCount() {
-		for (int i = 0; i < allMotors.length; i++) {
-			this.allMotors[i].resetTachoCount();
+		for (int i = 0; i < allMotors.size(); i++) {
+			this.allMotors.get(i).resetTachoCount();
 		}
 	}
 
-	public void setSpeed(int val) {
+	/**
+	 * Sets the operating speed of this ArmMotor.
+	 *
+	 * @param speed
+	 *            Speed, in number of degrees per seconds.
+	 */
+	public void setSpeed(int speed) {
 		this.synchronizeAllMotors();
-		for (int i = 0; i < allMotors.length; i++) {
-			this.allMotors[i].setSpeed(val);
+		for (int i = 0; i < allMotors.size(); i++) {
+			this.allMotors.get(i).setSpeed(speed);
 		}
-		this.stopSyncAndLaunchSynchronizedOperations(true);
+		this.stopSyncAndRunOperations(true);
 	}
 
+	/**
+	 * Causes this ArmMotor to stop, pretty much instantaneously by resisting
+	 * any further motion.
+	 */
 	public void stop() {
 		this.synchronizeAllMotors();
-		for (int i = 0; i < allMotors.length; i++) {
-			this.allMotors[i].stop(true);
+		for (int i = 0; i < allMotors.size(); i++) {
+			this.allMotors.get(i).stop(true);
 		}
-		this.stopSyncAndLaunchSynchronizedOperations(true);
+		this.stopSyncAndRunOperations(true);
+	}
+
+	/*
+	 * Arm synchronization
+	 */
+
+	/**
+	 * Returns the list of all motors this ArmMotor is made of, the master motor
+	 * included.
+	 *
+	 * @return List of all the motors this ArmMotor is made of.
+	 */
+	public ArrayList<BaseRegulatedMotor> getAllMotors() {
+		return this.allMotors;
+	}
+
+	/**
+	 * Starts a synchronized operation with another ArmMotor.
+	 *
+	 * @param otherArm
+	 *            The other ArmMotor this ArmMotor should synchronize with.
+	 */
+	public void synchronizeWithArm(ArmMotor otherArm) {
+		// Remember we're currently in the middle of a cross-Arm sync.
+		this.armSyncIsOn = true;
+
+		// Compute the list of all the slave motors to synchronize with.
+		this.crossArmSynchronizedSlaveMotors = this.slaveMotors;
+		this.crossArmSynchronizedSlaveMotors.addAll(otherArm.getAllMotors());
+
+		// Sync all motors with master one.
+		this.allMotors.get(0).synchronizeWith(
+				(RegulatedMotor[]) this.crossArmSynchronizedSlaveMotors
+						.toArray());
+	}
+
+	/**
+	 * Stops the synchronization process with another ArmMotor, and run all the
+	 * pending operations in a synchronized fashion.
+	 *
+	 * @param immediateReturn
+	 *            If true, method returns immediately. If false, waits for the
+	 *            operation to complete.
+	 */
+	public void stopSyncWithArmAndRunOperations(boolean immediateReturn) {
+		// Run operations.
+		this.allMotors.get(0).endSynchronization();
+
+		// Wait for operations to complete, if needed.
+		if (!immediateReturn) {
+			for (int i = 0; i < this.crossArmSynchronizedSlaveMotors.size(); i++) {
+				this.crossArmSynchronizedSlaveMotors.get(i).waitComplete();
+			}
+		}
+
+		// Disable synchronization.
+		this.armSyncIsOn = false;
+		this.crossArmSynchronizedSlaveMotors = new ArrayList<BaseRegulatedMotor>();
 	}
 
 	/*
 	 * Protected utility functions.
 	 */
 
+	/**
+	 * Starts a synchronized operation between all local motors of this
+	 * ArmMotor.
+	 */
 	protected void synchronizeAllMotors() {
-		this.allMotors[0].synchronizeWith(this.slaveMotors);
-
-		// Build the array of other motors.synchronizeWith(this.motors);
-
-		// TODO : When function do synchronize Arms between them is done, take
-		// the other Arm's motors into account in the list.
+		// Act only when a global sync between Arms is not in progress.
+		if (!this.armSyncIsOn) {
+			this.allMotors.get(0).synchronizeWith(
+					(RegulatedMotor[]) this.slaveMotors.toArray());
+		}
 	}
 
-	protected void stopSyncAndLaunchSynchronizedOperations(
-			boolean immediateReturn) {
-		this.allMotors[0].endSynchronization();
+	/**
+	 * Stops the synchronization process between all local motors of this
+	 * ArmMotor.
+	 *
+	 * @param immediateReturn
+	 *            If true, method returns immediately. If false, waits for the
+	 *            operation to complete.
+	 */
+	protected void stopSyncAndRunOperations(boolean immediateReturn) {
+		// Act only when a global sync between Arms is not in progress.
+		if (!this.armSyncIsOn) {
+			this.allMotors.get(0).endSynchronization();
 
-		if (!immediateReturn) {
-			for (int i = 0; i < allMotors.length; i++) {
-				this.allMotors[i].waitComplete();
+			if (!immediateReturn) {
+				for (int i = 0; i < allMotors.size(); i++) {
+					this.allMotors.get(i).waitComplete();
+				}
 			}
 		}
 	}
